81a7179a763c1b9b62efbb2cdd29817a17bb8ece
DAIRO  ARENAS <dairo.arenas802@pascualbravo.edu.co>
2025-02-26 22:47:36 -0500

    first commit

---
+++ Asignatura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNota
+{
+    public class Asignatura
+    {
+        public String nombre { get; set; }
+        public int credito { get; set; }
+        public List<String> nombreNota = new List<String>();
+        public List<double> valorNota = new List<double>();
+        public List<double> porcentajeNota = new List<double>();
+        public Dictionary<String, double> nota = new Dictionary<String, double>();
+        public Dictionary<String, double> asignaturaPonderada = new Dictionary<string, double>();
+
+        public Asignatura(String nombre, int credito)
+        {
+            this.nombre = nombre;
+            this.credito = credito;
+        }
+
+        public void CrearNota(String nombre, double valor, double porcentaje)
+        {
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConsoleAppNota")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppNota")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("a424ad3a-30be-4707-801a-2a7dbfe9a4ef")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
